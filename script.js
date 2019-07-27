fa6e5a902d9910582b77d050eed44331ee90d3e0
hanmonic <zhanghan1219work@gmail.com>
2019-07-27 22:20:10 +1000

    update

---
+++ script.js
@@ -0,0 +1,37 @@
+let listingImage = document.getElementById("listing-image");
+
+let nextListing = function() {
+    let randomListing = listings[Math.floor(Math.random() * 2)];
+    changeListingImage(randomListing);
+    changeUserDetails(randomListing);
+}
+
+let changeListingImage = function(listing) {
+    let imgNum = listing.listingNo;
+    listingImage.style.background = `url("./clothes${imgNum}.jpg")`;
+}
+
+let changeUserDetails = function(listing) {
+    let profilePic = document.getElementsByClassName("userprofile")[0];
+    let username = document.getElementById("username");
+    profilePic.style.backgroundColor = listing.userColor;
+    username.innerText = listing.userName;
+}
+
+listingImage.addEventListener("click", event => {
+    nextListing();
+});
+
+let listing0 = {
+    listingNo: 0,
+    userColor: "blue",
+    userName: "Joe Smith"
+}
+
+let listing1 = {
+    listingNo: 1,
+    userColor: "red",
+    userName: "John Smith"
+}
+
+let listings = [listing0, listing1];
