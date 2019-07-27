492fc72388190bb8a65a0676b6dd8d2e30cd313c
Vincent Wong <TheBrutux168@gmail.com>
2019-07-27 16:42:17 +1000

    More specific click listening in listing-image

--- script.js
+++ script.js
@@ -1,8 +1,17 @@
 let listingImage = document.getElementById("listing-image");
 
-listingImage.addEventListener("click", event => {
+let nextListing = function() {
     let imgNum = Math.floor(Math.random() * 2);
     listingImage.style.background = `url("./clothes${imgNum}.jpg")`;
-    console.log("test");
-});
+}
 
+listingImage.addEventListener("click", event => {
+    let x = event.pageX - listingImage.offsetLeft; 
+    let y = event.pageY - listingImage.offsetTop; 
+
+    if (x <= 50) {
+        nextListing();
+    } else if (x >= 400 && x <= 460) {
+        console.log("right");
+    }
+});
